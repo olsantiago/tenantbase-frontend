9191a9e015d6bea81fe6c220d9427a6d39ee92b3
olsantiago <44100716+olsantiago@users.noreply.github.com>
2019-08-02 11:46:37 -0700

    Add files via upload

---
+++ precache-manifest.ec982219122eb1e8748a48bdfe82da58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c301c9123e92879e49b3fda63c8fb69",
+    "url": "/tenantbase/index.html"
+  },
+  {
+    "revision": "9d4a61ff65a41161125e",
+    "url": "/tenantbase/static/css/main.c9ba7fa1.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/tenantbase/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "9d4a61ff65a41161125e",
+    "url": "/tenantbase/static/js/main.490311ee.chunk.js"
+  },
+  {
+    "revision": "b713869a9299580de5cc",
+    "url": "/tenantbase/static/js/runtime~main.326b0316.js"
+  }
+]);
